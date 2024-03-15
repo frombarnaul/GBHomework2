1b9ba74aef2b8504c0c879e4cd5b4e3e232f3888
frombarnaul <pajh5@yandex.ru>
2024-03-15 10:31:04 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace GBHomework2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] a = { { 7, 3, 2 }, { 4, 9, 6 }, { 1, 8, 5 } };
+
+            Sort2dArray(a);
+            Show2dArray(a);
+
+        }
+        static void Sort2dArray(int[,] array)
+        {
+            int[] newArr = array.Cast<int>().Order().ToArray();
+
+            for(int i = 0, newArrIndx = 0; i < array.GetLength(0); i++)
+            {
+                for(int j = 0;  j < array.GetLength(1); j++)
+                {
+                    array[i,j] = newArr[newArrIndx];
+                    newArrIndx++;
+                }
+            }
+
+        }
+        static void Show2dArray(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
